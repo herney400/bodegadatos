df5471115573b834ab2a61ab6d13f202c28379ac
N550J <N550J@ASUS>
2014-02-20 13:16:40 -0500

    Se creo clase Consultas.java

---
+++ bodegadatos/src/bodegadatos/Bodegadatos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bodegadatos;
+
+/**
+ *
+ * @author N550J
+ */
+public class Bodegadatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

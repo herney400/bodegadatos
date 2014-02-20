1310e65c917e0aacefef973293148ee0df13a2df
N550J <N550J@ASUS>
2014-02-20 16:23:12 -0500

    conectado

--- bodegadatos/src/bodegadatos/Bodegadatos.java
+++ bodegadatos/src/bodegadatos/Bodegadatos.java
@@ -6,6 +6,8 @@
 
 package bodegadatos;
 
+import java.sql.SQLException;
+
 /**
  *
  * @author N550J
@@ -15,8 +17,11 @@ public class Bodegadatos {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // TODO code application logic here
+        
+        FachadaDW f=new FachadaDW();
+        f.conexion();
     }
     
 }
